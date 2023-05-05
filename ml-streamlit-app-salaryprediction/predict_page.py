363a2df15c96eff0fe42557ea54778376c394f46
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-05 14:25:19 +1000

    Update predict_page.py

--- ml-streamlit-app-salaryprediction/predict_page.py
+++ ml-streamlit-app-salaryprediction/predict_page.py
@@ -190,7 +190,7 @@ def show_predict_page():
         X['recruiter'] = jobClassification_enc.fit_transform(X['recruiter'])
         X['teaser'] = preprocess_text_input(X['teaser'])
         X['desktopAdTemplate'] = preprocess_text_input(X['desktopAdTemplate'])
-        teaser_tfidf = teaser_tfidf.reshape(1, -1).toarray()
+        teaser_tfidf = teaser_tfidf.reshape(1, -1).toarray() if isinstance(teaser_tfidf, scipy.sparse.csr.csr_matrix) else teaser_tfidf.reshape(1, -1)
         desktopAdTemplate_tfidf = desktopAdTemplate_tfidf.reshape(1, -1).toarray()
 
         # Concatenate the TF-IDF vectors with the original dataframe
