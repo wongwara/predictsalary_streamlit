772800da5063bb226af4be6000a9e774a389e911
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-10 17:04:52 +1000

    Update app.py

--- ml-streamlit-app-salaryprediction/app.py
+++ ml-streamlit-app-salaryprediction/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from predict_page import show_predict_page
+from explore_page import show_explore_page
 import nltk
+
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
 
 # Page configuration
 st.set_page_config(
@@ -10,7 +18,7 @@ st.set_page_config(
      initial_sidebar_state='expanded')
 
 # Title of the app
-st.title('🌷 Salary Prediction App')
+st.title('Salary Prediction App')
 show_predict_page()
 
     
