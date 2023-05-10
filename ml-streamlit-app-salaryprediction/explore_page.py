0845f521afc634366946192fee3f1b691b2af68a
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-10 17:26:06 +1000

    Update explore_page.py

--- ml-streamlit-app-salaryprediction/explore_page.py
+++ ml-streamlit-app-salaryprediction/explore_page.py
@@ -11,19 +11,11 @@ df = load_data()
 
 def show_explore_page():
     st.title("Explore Software Engineer Salaries")
+    st.dataframe(data=df, width=50, height=20, *, use_container_width=False)
 
     st.write(
         """
     ### Jobs released each month from January 2019 to January 2022
     """
     )
-    df.index = pd.to_datetime(df['listingDate'])
-    monthly_count = df.resample('W').size()
-    monthly_count = monthly_count.reset_index(name = 'count')
-    chart_data = {'x': monthly_count['listingDate'], 'y': monthly_count['count']}
-    st.line_chart(chart_data)
     
-#     st.set_title('Job release amount per week')
-#     ax.tick_params(axis='x', rotation=45)
-
-#     st.pyplot(fig1)
