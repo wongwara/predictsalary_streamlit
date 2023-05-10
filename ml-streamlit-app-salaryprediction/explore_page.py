534134288a18a21cdb9fe90a63cf60bda0b96d31
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-10 17:50:55 +1000

    Update explore_page.py

--- ml-streamlit-app-salaryprediction/explore_page.py
+++ ml-streamlit-app-salaryprediction/explore_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 @st.cache
 def load_data():
@@ -10,12 +11,21 @@ def load_data():
 df = load_data()
 
 def show_explore_page():
-    st.title("Job salary for data scientist in AUSTRALIA")
+    st.title("💰 Job salary for data scientist in AUSTRALIA")
     
-    st.table(df)
+    
     st.write(
         """
     ### Jobs released each month from January 2019 to January 2022
     """
     )
-    
+    st.subheader("The highest demand job classification in Australia")
+    # Compute the counts for each job classification
+    job_counts = df['jobClassification'].value_counts()
+
+    # Create a Plotly bar chart
+    fig = px.bar(x=job_counts.index, y=job_counts.values, color=job_counts.index)
+    fig.update_layout(xaxis_title='Job Classification', yaxis_title='Count', title='Highest Demand Job Classification')
+
+    # Display the chart in Streamlit
+    st.plotly_chart(fig)
